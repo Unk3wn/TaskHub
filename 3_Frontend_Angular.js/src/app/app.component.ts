65ca1f8a434936b9ddd0fa41972abf829497a89f
Nico <holzhaeuser.nico@student.dhbw-karlsruhe.de>
2020-11-23 18:55:03 +0100

    :spark: Added Login to Angular Component Props an Danny

--- 3_Frontend_Angular.js/src/app/app.component.ts
+++ 3_Frontend_Angular.js/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { AccountService } from '../app/_services/account.service';
+import { User } from '../app/_models/user';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
 export class AppComponent {
-  title = 'Frontend';
+  user: User;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user.subscribe(x => this.user = x);
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
 }
