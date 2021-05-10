72449cd23ec9ac2522dda7d6d7be3d92b93ab6e6
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-10 22:17:47 +0200

    :sparkles: Clean app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,75 +1,36 @@
-const createError = require('http-errors');
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcryptjs');
-const dotenv = require('dotenv');
+const express = require('express'),
+      bcrypt = require('bcryptjs'),
+      dotenv = require('dotenv'),
+      path = require('path'),
+      logger = require('morgan'),
+      indexRouter = require('./routes/index'),
+      app = express();
+
+//===============PASSPORT===============
+
+//===============EXPRESS================
+app.use(logger('dev'));
 dotenv.config();
 
-const cors = require('cors');
-const corsOptions = {
-  origin: "http://localhost:"+process.env.PORT
-};
-
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(cors(corsOptions));
-// parse requests of content-type - application/json
-app.use(bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+//===============ROUTES===============
 app.use('/', indexRouter);
-/* simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
-});*/
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-const db = require("./sequelize/models");
-
-//DEV
-if (process.env.NODE_ENV == "development"){
-  db.sequelize.sync({force: true,logging: console.log}).then(() => {
-    console.log('Drop and Resync Db');
-    initzialize();
-  });
-}
-//PROD
-else if(process.env.NODE_ENV == "production"){
-  db.sequelize.sync();
-}
+//===============SEQUELIZE===============
+  const db = require("./sequelize/models");
+  //DEV
+  if (process.env.NODE_ENV == "development"){
+    db.sequelize.sync({force: true,logging: console.log}).then(() => {
+      console.log('Drop and Resync Db');
+      initzialize();
+    });
+  }
+  //PROD
+  else if(process.env.NODE_ENV == "production"){
+    db.sequelize.sync();
+  }
 
 module.exports = app;
-
 
 function initzialize(){
 
