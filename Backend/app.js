5019077ca652ac5ff3248ef10686444ccb0f1aa3
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-13 12:47:21 +0200

    :sparkles: Added Cors + Logging Options for Sequelize

--- Backend/app.js
+++ Backend/app.js
@@ -1,34 +1,50 @@
 const express = require('express'),
       bcrypt = require('bcryptjs'),
       dotenv = require('dotenv'),
-      path = require('path'),
       logger = require('morgan'),
       indexRouter = require('./routes/index'),
+      bodyParser = require('body-parser'),
+      cors = require('cors')
+
       app = express();
 
 //===============PASSPORT===============
 
+//===============CORS===============
+const corsOptions = {
+  origin: "http://localhost:"+process.env.PORT
+};
+app.use(cors(corsOptions));
+
 //===============EXPRESS================
 app.use(logger('dev'));
 dotenv.config();
+
+//===============URL BODY================
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 //===============ROUTES===============
 app.use('/', indexRouter);
 
 
 //===============SEQUELIZE===============
-  const db = require("./sequelize/models");
-  //DEV
-  if (process.env.NODE_ENV == "development"){
-    db.sequelize.sync({force: true,logging: console.log}).then(() => {
-      console.log('Drop and Resync Db');
-      initzialize();
-    });
-  }
-  //PROD
-  else if(process.env.NODE_ENV == "production"){
-    db.sequelize.sync();
-  }
+const db = require("./sequelize/models");
+//DEV
+if (process.env.NODE_ENV == "development"){
+  db.sequelize.sync({force: true,logging: console.log}).then(() => {
+    console.log('Drop and Resync Db');
+    initzialize();
+  });
+}
+//PROD
+else if(process.env.NODE_ENV == "production"){
+  db.sequelize.options.logging = false;
+  db.sequelize.sync();
+}
 
 module.exports = app;
 
