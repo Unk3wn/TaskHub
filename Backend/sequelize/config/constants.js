ca56763f1f517f9be8fc387660045998b34b3d64
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-23 13:15:28 +0200

    :sparkles: Added more UUID's for Seeding

--- Backend/sequelize/config/constants.js
+++ Backend/sequelize/config/constants.js
@@ -1,6 +1,10 @@
 // File: constants.js
 module.exports = {
     roleID : '097ed6cc-657a-4c6d-9f0f-d4fadb1fced3',
+    roleIDAdmin : '2046a519-138a-49f6-b4e8-e426b619d6f7',
+    roleIDTeacher : 'e2c39c97-7ff2-4e23-8295-a9f67d4d7c3d',
+    roleIDUser : 'd411c415-6540-4310-b7d0-122cc292bb52',
+    roleIDStudent : '5c62594f-f973-4d28-bfd7-aa18679ebdf0',
     subjectID: 'eb1a1ae0-35e2-47b7-919e-9042704f059b',
     classID: '7a2fb42d-2fcf-4c6e-b285-c9fd8f8b4f73',
     userID: '3f7bf944-7a3f-4648-bfec-29d8a5722895',
