03f59644af841d16843675b7a5b6859f97690082
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-23 13:00:31 +0200

    :sparkles: Added Auth Route

--- Backend/routes/apiRouter.js
+++ Backend/routes/apiRouter.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.use('/user',require('./api/userRouter'));
 router.use('/role',require('./api/roleRouter'));
@@ -8,5 +8,12 @@ router.use('/class',require('./api/classRouter'));
 router.use('/task',require('./api/taskRouter'));
 router.use('/team',require('./api/teamRouter'));
 router.use('/solution',require('./api/userRouter'));
+
+// AUTH
+router.use('/auth',require('./api/authRouter'));
+
+// Test for Authentification
+router.use('/test',require('./api/authTestRouter'));
+
 
 module.exports = router;
