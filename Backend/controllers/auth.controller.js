db6fdb30da13130d8f02a962c598fb4726f3979a
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-23 14:13:45 +0200

    :sparkles: Added Controllers for the Middleware

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,90 @@
+const db = require("../sequelize/models");
+const User = db.user;
+const Role = db.role;
+
+const Op = db.Sequelize.Op;
+
+var jwt = require("jsonwebtoken");
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+
+exports.signup = (req, res) => {
+    // Save User to Database
+    User.create({
+        user_id : uuid.v4(),
+        username : req.body.username,
+        password : bcrypt.hashSync(req.body.password,10),
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        email : req.body.email
+    })
+        .then(user => {
+            if (req.body.roles) {
+                Role.findAll({
+                    where: {
+                        name: {
+                            [Op.or]: req.body.roles
+                        }
+                    }
+                }).then(roles => {
+                    user.setRoles(roles).then(() => {
+                        res.send({ message: "User was registered successfully!" });
+                    });
+                });
+            } else {
+                // user role = 1
+                user.setRoles([1]).then(() => {
+                    res.send({ message: "User was registered successfully!" });
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
+};
+
+exports.signin = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+        .then(user => {
+            if (!user) {
+                return res.status(404).send({ message: "User Not found." });
+            }
+
+            var passwordIsValid = bcrypt.compareSync(
+                req.body.password,
+                user.password
+            );
+
+            if (!passwordIsValid) {
+                return res.status(401).send({
+                    accessToken: null,
+                    message: "Invalid Password!"
+                });
+            }
+
+            const token = jwt.sign({ user_id: user.user_id }, process.env.SESSION_SECRET, {
+                expiresIn: 86400 // 24 hours
+            });
+
+            var authorities = [];
+            user.getRoles().then(roles => {
+                for (let i = 0; i < roles.length; i++) {
+                    authorities.push("ROLE_" + roles[i].role_name.toUpperCase());
+                }
+                res.status(200).send({
+                    id: user.id,
+                    username: user.username,
+                    email: user.email,
+                    roles: authorities,
+                    accessToken: token
+                });
+            });
+        })
+        .catch(err => {
+            res.status(500).send({ message: err.message });
+        });
+};
