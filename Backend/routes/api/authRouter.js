725d6cc1e2441f989cfa82a60f74936667c7e625
Unk3wn <holzhaeuser.nico@gmail.com>
2021-05-23 14:13:16 +0200

    :sparkles: Added Routes

--- Backend/routes/api/authRouter.js
+++ Backend/routes/api/authRouter.js
@@ -1,7 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+const {verifySignUp} = require("../../middelware/index");
+const controller = require("../../controllers/auth.controller");
 
+router.use(function (req, res, next) {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+})
+router.post("/signup",
+    [
+        verifySignUp.checkDuplicateUsernameOrEmail,
+        verifySignUp.checkRolesExisted
+    ],
+    controller.signup);
 
+router.post("/signin", controller.signin);
 
 module.exports = router;
