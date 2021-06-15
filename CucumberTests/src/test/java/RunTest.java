820b6ca074547fbb06d9e82d0f1ebbe026de6ac0
Krissi <blauerelefant005@gmail.com>
2021-06-15 15:17:28 +0200

    TH-27 Adapt imports for cucumber test files

--- CucumberTests/src/test/java/RunTest.java
+++ CucumberTests/src/test/java/RunTest.java
@@ -1,11 +1,28 @@
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import stepdefs.Preconditions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resource/"}
+        features = { "src/test/resource/" }
 )
 
 public class RunTest {
+    @BeforeClass
+    public static void setup() {
+        Preconditions.driver = new FirefoxDriver();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        if (Preconditions.driver != null) {
+            Preconditions.driver.close();
+        }
+    }
 }
